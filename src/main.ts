1c3bb7b86660005118584d26587cdc0c109e5f6f
Julius Nerlich <j.nerlich@finatix.de>
2026-01-07 14:34:05 +0100

    grobes Game Layout und View Navigation zwischen Game-View und Leaderboard-View(Ticket 1)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,37 @@
 import "./styles/main.css";
 
+import {mountLayout} from "./ui/layout";
+import {renderGameView} from "./ui/gameView";
+import {renderLeaderboardView} from "./ui/leaderboardView";
+
 const root = document.querySelector<HTMLDivElement>("#app");
 if (!root) throw new Error("#app not found");
 
-root.innerHTML = `
-  <h1 class="text-3xl font-bold text-emerald-400">
-    Hello Snake
-  </h1>
-`;
+//Layout mounten
+const ui = mountLayout(root);
+
+//zentrale View-Wechselfunktion
+function showView(view: "game" | "leaderboard") {
+    ui.viewRoot.innerHTML = ``;
+
+    if (view === "game") {
+        renderGameView(ui.viewRoot);
+    }
+
+    if (view === "leaderboard") {
+        renderLeaderboardView(ui.viewRoot);
+    }
+}
+
+// Event listener für Navigation
+ui.navGame.addEventListener("click", () => {
+    showView("game");
+});
+ui.navLeaderboard.addEventListener("click", () => {
+    showView("leaderboard");
+});
+
+//Startpunkt
+showView("game");
+
+
