c2e1932df3aaa5d19e4414f364c0a48fdf0d2bdd
Julius Nerlich <j.nerlich@finatix.de>
2026-01-09 15:55:06 +0100

    game loop in engine erstellt und tick funktion in main implementiert

---
+++ src/game/engine.ts
@@ -0,0 +1,46 @@
+import type { GameState, Point } from "./state"
+
+export function tick (state: GameState) {
+    const head = state.snake[0];
+    let newHead: Point;
+    switch (state.direction) {
+        case "left":
+            newHead = {
+                x: head.x - 1,
+                y: head.y,
+            };
+            break;
+
+        case "right":
+            newHead = {
+                x: head.x + 1,
+                y: head.y,
+            };
+            break;
+
+        case "up":
+            newHead = {
+                x: head.x,
+                y: head.y - 1,
+            };
+            break;
+
+        case "down":
+            newHead = {
+                x: head.x,
+                y: head.y + 1,
+            };
+
+            break;
+    }
+
+    const newSnake = [
+        newHead,
+        ...state.snake.slice(0, -1),
+    ];
+
+    return{
+        ...state,
+        snake: newSnake,
+    };
+}
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import {mountLayout} from "./ui/layout";
 import {renderGameView} from "./ui/gameView";
 import {renderLeaderboardView} from "./ui/leaderboardView";
 import {createKonvaRenderer, type KonvaRenderer} from "./game/rendererKonva";
-import {getTestState, type GameState} from "./game/state";
+import {getTestState, initGameState, type GameState} from "./game/state";
+import {tick} from "./game/engine";
 
 type ViewName = "game" | "leaderboard";
 
@@ -14,13 +15,19 @@ if (!root) throw new Error("#app not found");
 //Layout mounten
 const ui = mountLayout(root);
 
+let gameState: GameState | null = null;
+let gameInterval: number | null = null;
 let renderer: KonvaRenderer<GameState> | null = null;
 
 function cleanupCurrentView() {
-    if (renderer) {
-        renderer.destroy();
-        renderer = null;
+    if (gameInterval !== null) {
+        clearInterval(gameInterval);
+        gameInterval = null;
     }
+
+    renderer?.destroy();
+    renderer = null;
+    gameState = null;
 }
 
 //zentrale View-Wechselfunktion
@@ -34,6 +41,22 @@ function showView(view: ViewName) {
     //Neue View rendern
     if (view === "game") {
         const gameUI = renderGameView(ui.viewRoot);
+        gameUI.startButton.addEventListener("click", () => {
+            if (gameInterval !== null) return;
+
+            gameState = initGameState();
+
+            renderer = createKonvaRenderer<GameState>(gameUI.gameContainer);
+            renderer.drawGrid(gameState);
+            renderer.draw(gameState);
+
+            gameInterval = window.setInterval(() => {
+                if (!gameState || !renderer) return;
+
+                gameState = tick(gameState);
+                renderer.draw(gameState);
+            }, 150)
+        })
 
         //Konva initialisieren
         renderer = createKonvaRenderer<GameState>(gameUI.gameContainer);
