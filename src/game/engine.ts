eebc1166bf79a9c5614b31f5f71db21ced49783a
Julius Nerlich <j.nerlich@finatix.de>
2026-01-13 13:39:57 +0100

    engine now parted into 3 functions: isOpposite (to implement 180° rule), nextHead (to calculate the psition of a the new Head) and loop (exporting the new state with a new direction and snake, as well as clearing the pendingDirection property)

--- src/game/engine.ts
+++ src/game/engine.ts
@@ -1,40 +1,50 @@
-import type { GameState, Point } from "./state"
+import type {Direction, GameState, Point} from "./state"
+import {createKonvaRenderer} from "./rendererKonva";
 
-export function tick (state: GameState) {
+function isOpposite (a: Direction, b: Direction): boolean {
+    return (
+        (a === "up" || b === "down") ||
+        (a === "down" || b === "up") ||
+        (a === "left" || b === "right") ||
+        (a === "right" || b === "left")
+    );
+}
+
+function nextHead (head: {x: number, y:number}, dir: Direction) {
+    switch (dir) {
+        case "left":
+            return{
+                x: head.x - 1,
+                y: head.y,
+            };
+
+        case "right":
+            return{
+                x: head.x + 1,
+                y: head.y,
+            };
+
+        case "up":
+            return{
+                x: head.x,
+                y: head.y - 1,
+            };
+
+        case "down":
+            return{
+                x: head.x,
+                y: head.y + 1,
+            };
+    }
+}
+
+export function tick (state: GameState): GameState{
+    const desired = state.pendingDirection;
+    const direction =
+        desired && !isOpposite(desired, state.direction) ? desired : state.direction;
+
     const head = state.snake[0];
-    let newHead: Point;
-
-        switch (state.direction) {
-            case "left":
-                newHead = {
-                    x: head.x - 1,
-                    y: head.y,
-                };
-                break;
-
-            case "right":
-                newHead = {
-                    x: head.x + 1,
-                    y: head.y,
-                };
-                break;
-
-            case "up":
-                newHead = {
-                    x: head.x,
-                    y: head.y - 1,
-                };
-                break;
-
-            case "down":
-                newHead = {
-                    x: head.x,
-                    y: head.y + 1,
-                };
-
-                break;
-        }
-
+    const newHead = nextHead(head, direction);
     const newSnake = [
         newHead,
         ...state.snake.slice(0, -1),
@@ -42,6 +52,8 @@ export function tick (state: GameState) {
 
     return{
         ...state,
+        direction,
+        pendingDirection: undefined,
         snake: newSnake,
     };
 }
