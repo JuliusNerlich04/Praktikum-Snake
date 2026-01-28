a20b6bfdd623d3020dd84e59c64be8aaba49749b
Julius Nerlich <j.nerlich@finatix.de>
2026-01-28 11:17:00 +0100

    Leaderboard logic implemented in main and leaderboardRenderer now displays the top 10

--- src/ui/leaderboardView.ts
+++ src/ui/leaderboardView.ts
@@ -1,18 +1,64 @@
+import {loadLeaderboard, type LeaderboardEntry} from "../storage/leaderboardStore";
+
+
 export function renderLeaderboardView(container : HTMLElement) {
-    container.innerHTML = `
-        <table>
-            <tr>
-                <th>Name</th>
-                <th>Score</th>
-            </tr>
-            <tr>
-                <td>Max</td>
-                <td>100</td>
-            </tr>
-            <tr>
-                <td>Julius</td>
-                <td>99</td>
-            </tr>
-        </table>
-    `
+    const entries: LeaderboardEntry[] = loadLeaderboard();
+    container.innerHTML = ""
+    if (entries.length === 0) {
+        const p = document.createElement("p");
+        p.textContent = 'noch keine Einträge'
+        container.appendChild(p);
+        return;
+    }
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const thRow = document.createElement("tr");
+    const thRang = document.createElement("th");
+    thRang.textContent = 'Rang'
+    const thName = document.createElement("th");
+    thName.textContent = 'Name'
+    const thScore = document.createElement("th");
+    thScore.textContent = 'Score'
+    const thDatum = document.createElement("th");
+    thDatum.textContent = 'Datum'
+
+    table.classList.add("w-full", "border-collapse")
+    thRow.classList.add("border-b")
+    thRang.classList.add("p-2", "text-left", "font-semibold");
+    thName.classList.add("p-2", "text-left", "font-semibold");
+    thScore.classList.add("p-2", "text-left", "font-semibold");
+    thDatum.classList.add("p-2", "text-left", "font-semibold");
+
+    thRow.append(thRang);
+    thRow.append(thName);
+    thRow.append(thScore);
+    thRow.append(thDatum);
+    thead.append(thRow);
+
+    table.append(thead);
+
+    const tbody = document.createElement("tbody");
+    for (let i = 0, len = entries.length; i < len; i++) {
+        const entry = entries[i];
+        const trRow = document.createElement("tr");
+        const tdRang = document.createElement("td");
+        tdRang.textContent = (i + 1).toString();
+        const tdName = document.createElement("td");
+        tdName.textContent = entry.name;
+        const tdScore = document.createElement("td");
+        tdScore.textContent = entry.score.toString();
+        const tdDatum = document.createElement("td");
+        tdDatum.textContent = new Date(entry.dateISO).toLocaleString("de-DE");
+
+        tdRang.classList.add("p-2", "border-b");
+        tdName.classList.add("p-2", "border-b");
+        tdScore.classList.add("p-2", "border-b");
+        tdDatum.classList.add("p-2", "border-b");
+
+        trRow.append(tdRang, tdName, tdScore, tdDatum);
+        tbody.append(trRow);
+    }
+    table.append(tbody);
+
+    container.append(table);
 }
